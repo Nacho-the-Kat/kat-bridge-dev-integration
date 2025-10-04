2ce5ea54e5e382c750b44c88c35e1ec5dbbe2be2
ktqappi <kt@alphaguilty.io>
2025-10-04 17:03:11 +0600

    feat: added p2sh decoder

---
+++ kaspa-wasm32-sdk/package-sizes.js
@@ -0,0 +1,10 @@
+window.packageSizes = {
+  "kaspa": "11.34 MB",
+  "kaspa-core": "11.34 MB",
+  "kaspa-core-dev": "42.68 MB",
+  "kaspa-dev": "42.68 MB",
+  "kaspa-keygen": "1.71 MB",
+  "kaspa-keygen-dev": "3.42 MB",
+  "kaspa-rpc": "3.38 MB",
+  "kaspa-rpc-dev": "17.59 MB"
+};
